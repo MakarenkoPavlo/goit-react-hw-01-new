c4e115c12340ee03eea105592c86d467872a7f25
Makarenko Pavlo <cmertnik1986@gmail.com>
2024-01-30 00:07:43 +0200

    hw1 finish

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,10 +1,10 @@
 import css from './FriendListItem.module.css'
 export const FriendListItem = ({ avatar, name, isOnline }) => {
+ const statusClass = isOnline ? css.Online : css.Offline
     return (
     <div className={css.item}>
         <img className={css.avatar} src={avatar} alt="Avatar" width="64" />
         <p className={css.name}>{name}</p>
-        <p className={css.status}>{isOnline ? 'Online' : 'Offline'}</p>
-    </div>
+        <p className={statusClass}>{isOnline ? 'Online' : 'Offline'}</p>    </div>
     )
 }
